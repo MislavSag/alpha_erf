823dd8a4635758421261f85a7ffbb96c5797d702
MislavSag <mislav.sagovac@contentio.biz>
2024-11-05 13:37:29 +0100

    installation problems

--- estimate.R
+++ estimate.R
@@ -17,6 +17,7 @@ i = as.integer(Sys.getenv('PBS_ARRAY_INDEX'))
 
 # Import data
 print(paste0("Import data for index", i))
+print(list.files("."))
 dt = open_dataset("/home/jmaric/alpha_erf/data/prices.csv", format = "csv") |>
   dplyr::filter(id == i) |>
   collect()
