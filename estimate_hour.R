0fd0ddbfbd8cb0beaa0cc8299c1ec0ee3e417c72
MislavSag <mislav.sagovac@contentio.biz>
2024-11-14 16:16:03 +0100

    solve return in for loop

--- estimate_hour.R
+++ estimate_hour.R
@@ -37,63 +37,70 @@ predictors = dt[, colnames(.SD),
 # Check if already estimated
 s = dt[, data.table::first(symbol)]
 file_name = file.path(SAVEPATH, paste0(s, ".csv"))
-if (file.exists(file_name)) next
-
-print(dt)
-print(s)
-print(dates)
-
-# estimation
-l = list()
-for (i in 1:length(dates)) { # 1:length(dates
-  d = dates[i]
-  # d = dates[10000]
+if (file.exists(file_name)) {
+  print("Already estimated")
+} else {
+  # estimation
+  l = list()
+  for (i in 1:length(dates)) { # 1:length(dates
+    d = dates[i]
+    # d = dates[10000]
+    
+    # Train data
+    dtd = dt[date < d]
+    if (nrow(dtd) < 7 * 252) {
+      l[[i]] = NULL
+      next
+    } 
+    if (d - dt[, max(date)] > 1) {
+      l[[i]] = NULL
+      next
+    }
+    
+    # Test data
+    test_data = dt[date == d]
+    if (nrow(test_data) == 0) {
+      l[[i]] = NULL
+      next
+    }
+    
+    # Fit model for upper
+    train_data_upper = dtd[target > 0]
+    erf_model_upper = erf(
+      X = as.matrix(train_data_upper[, .SD, .SDcols = predictors]),
+      Y = train_data_upper[, target],
+      min.node.size = 5,
+      lambda = 0.001,
+      intermediate_quantile = 0.8
+    )
+    
+    # Fit model for lower
+    train_data_lower = dtd[target < 0]
+    erf_model_lower = erf(
+      X = as.matrix(train_data_lower[, .SD, .SDcols = predictors]),
+      Y = -train_data_lower[, target],
+      min.node.size = 5,
+      lambda = 0.001,
+      intermediate_quantile = 0.8
+    )
+    
+    # Predict
+    erf_predictions_upper = predict(erf_model_upper, as.matrix(test_data[, .SD, .SDcols = predictors]), quantiles = quantile_levels)
+    erf_predictions_lower = predict(erf_model_lower, as.matrix(test_data[, .SD, .SDcols = predictors]), quantiles = quantile_levels)
+    erf_predictions_upper = clean_names(as.data.frame(erf_predictions_upper))
+    colnames(erf_predictions_upper) = paste0("upper_", colnames(erf_predictions_upper))
+    erf_predictions_lower = clean_names(as.data.frame(erf_predictions_lower))
+    colnames(erf_predictions_lower) = paste0("lower_", colnames(erf_predictions_lower))
+    l[[i]] = cbind(
+      symbol = s,
+      date = d,
+      erf_predictions_upper,
+      erf_predictions_lower,
+      targetr = test_data[, target]
+    )
+  }
   
-  # Train data
-  dtd = dt[date < d]
-  if (nrow(dtd) < 7 * 252) return(NULL)
-  if (d - dt[, max(date)] > 1) return(NULL)
-  
-  # Test data
-  test_data = dt[date == d]
-  if (nrow(test_data) == 0) return(NULL)
-  
-  # Fit model for upper
-  train_data_upper = dtd[target > 0]
-  erf_model_upper = erf(
-    X = as.matrix(train_data_upper[, .SD, .SDcols = predictors]),
-    Y = train_data_upper[, target],
-    min.node.size = 5,
-    lambda = 0.001,
-    intermediate_quantile = 0.8
-  )
-  
-  # Fit model for lower
-  train_data_lower = dtd[target < 0]
-  erf_model_lower = erf(
-    X = as.matrix(train_data_lower[, .SD, .SDcols = predictors]),
-    Y = -train_data_lower[, target],
-    min.node.size = 5,
-    lambda = 0.001,
-    intermediate_quantile = 0.8
-  )
-  
-  # Predict
-  erf_predictions_upper = predict(erf_model_upper, as.matrix(test_data[, .SD, .SDcols = predictors]), quantiles = quantile_levels)
-  erf_predictions_lower = predict(erf_model_lower, as.matrix(test_data[, .SD, .SDcols = predictors]), quantiles = quantile_levels)
-  erf_predictions_upper = clean_names(as.data.frame(erf_predictions_upper))
-  colnames(erf_predictions_upper) = paste0("upper_", colnames(erf_predictions_upper))
-  erf_predictions_lower = clean_names(as.data.frame(erf_predictions_lower))
-  colnames(erf_predictions_lower) = paste0("lower_", colnames(erf_predictions_lower))
-  l[[i]] = cbind(
-    symbol = s,
-    date = d,
-    erf_predictions_upper,
-    erf_predictions_lower,
-    targetr = test_data[, target]
-  )
+  # Clean and save
+  x = rbindlist(l)
+  fwrite(x, file_name)
 }
-  
-# Clean and save
-x = rbindlist(l)
-fwrite(x, file_name)
